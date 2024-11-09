1906ede044516980faf0adb6fbf707b9c2a84026
Laughing-q <1185102784@qq.com>
2024-11-09 13:39:52 +0800

    Update test_imx500.py

--- tests/test_imx500.py
+++ tests/test_imx500.py
@@ -2,11 +2,11 @@ from ultralytics import YOLO
 from tests import SOURCE, MODEL
 
 
-def test_export_tflite():
-    """Test YOLO exports to TFLite format under specific OS and Python version conditions."""
-    model = YOLO(MODEL)
-    file = model.export(format="tflite", imgsz=32)
-    YOLO(file)(SOURCE, imgsz=32)
+# def test_export_tflite():
+#     """Test YOLO exports to TFLite format under specific OS and Python version conditions."""
+#     model = YOLO(MODEL)
+#     file = model.export(format="tflite", imgsz=32)
+#     YOLO(file)(SOURCE, imgsz=32)
 
 
 def test_export_imx500_ptq():
